f9cd4c4d5e272a4cc576678b48fcadb52efa1526
Nikolaos Kitsis <kitsisnick@gmail.com>
2023-10-14 17:48:38 +0300

    Exercise: Add lines

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fpdf import FPDF
 import pandas as pd
 
-
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False, margin=0)
 
@@ -15,6 +14,9 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 21, 200, 21)
+
+    for i in range(30, 298, 10):
+        pdf.line(10, i, 200, i)
 
     # Set the footer
     pdf.ln(265)  # 265mm empty line
@@ -30,5 +32,8 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        for j in range(20, 298, 10):
+            pdf.line(10, j, 200, j)
 
 pdf.output("output.pdf")
